ffd36f11464d1aff24bc49921098adbd548d2617
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2022-08-05 08:06:00 +0200

    fix: remove printing commands on startup of the container

--- containers/agent/entrypoint.sh
+++ containers/agent/entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-set -xeuo pipefail
+set -euo pipefail
 
+echo "Setting the Git credential helper store to /secrets/git-credentials"
 git config --global credential.helper "store --file=/secrets/git-credentials"
 
 if [[ -f /secrets/ssh-key ]]; then
+  echo "Importing SSH key at /secrets/ssh-key"
   eval "$(ssh-agent -s)"
   ssh-add -k /secrets/ssh-key
 fi
 
 if [[ -d /local ]]; then
+  echo "Copying buildkite-agent binary to /local"
   cp /usr/local/bin/buildkite-agent /local/
 fi
 
